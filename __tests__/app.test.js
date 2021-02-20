2a10077192edd1a7a88390ae497169bb54ec20dc
Cierra-McDonald <cierralauren15@gmail.com>
2021-02-19 17:33:21 -0700

    test work!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,35 +31,101 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns plants', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          id: 1, 
+          image: 'silver-evergreen.png',
+          genus: 'Aglaonema', 
+          size: 'Medium',
+          light: 'Low Light',
+          price: '60',
+          name: 'Silver Evergreen',
+          owner_id: 1
+        }, 
+        {
+          id: 2, 
+          image: 'dragon-tree.png',
+          genus: 'Dracaena marginata',
+          size: 'Medium',
+          light: 'Medium Light',
+          price: '25',
+          name: 'Dragon Tree',
+          owner_id: 1
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          id: 3, 
+          image: 'mother-tongue.png',
+          genus: 'Sansevieria trifasciata',
+          size: 'Medium',
+          light: 'Medium Light',
+          price: '40',
+          name: 'Mother-in-laws Tongue',
+          owner_id: 1
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          id: 4, 
+          image: 'peace-lily.png',
+          genus:  'Spathiphyllum',
+          size: 'Large',
+          light: 'High Light',
+          price: '30',
+          name:  'Peace - Lily',
+          owner_id: 1
+        },
+        {
+          id: 5, 
+          image: 'rubber-tree.png',
+          genus: 'Ficus elastica',
+          size: 'Medium',
+          light: 'Low Light',
+          price: '35',
+          name: 'Rubber Tree',
+          owner_id: 1
+        },
+        {
+          id: 6, 
+          image: 'aloe-vera.png',
+          genus: 'Aloe barbadenis Miller',
+          size: 'Medium',
+          light: 'High Light',
+          price: '15',
+          name: 'Aloe Vera',
+          owner_id: 1
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/plants')
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
+
+    });
+
+    test('returns a single plant with the matching id', async() => {
+
+      const expectation = {
+        id: 4, 
+        image: 'peace-lily.png',
+        genus:  'Spathiphyllum',
+        size: 'Large',
+        light: 'High Light',
+        price: '30',
+        name:  'Peace - Lily',
+        owner_id: 1
+      };
+  
+      const data = await fakeRequest(app)
+        .get('/plants/4')
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(data.body[0]).toEqual(expectation);
+
+
     });
   });
 });
---
+++ data/plants.js
@@ -0,0 +1,57 @@
+module.exports = [
+  {
+    id: 1, 
+    image: 'silver-evergreen.png',
+    genus: 'Aglaonema', 
+    size: 'Medium',
+    light: 'Low Light',
+    price: '60',
+    name: 'Silver Evergreen'
+  }, 
+  {
+    id: 2, 
+    image: 'dragon-tree.png',
+    genus: 'Dracaena marginata',
+    size: 'Medium',
+    light: 'Medium Light',
+    price: '25',
+    name: 'Dragon Tree'
+  },
+  {
+    id: 3, 
+    image: 'mother-tongue.png',
+    genus: 'Sansevieria trifasciata',
+    size: 'Medium',
+    light: 'Medium Light',
+    price: '40',
+    name: 'Mother-in-laws Tongue'
+  },
+  {
+    id: 4, 
+    image: 'peace-lily.png',
+    genus:  'Spathiphyllum',
+    size: 'Large',
+    light: 'High Light',
+    price: '30',
+    name:  'Peace - Lily'
+  },
+  {
+    id: 5, 
+    image: 'rubber-tree.png',
+    genus: 'Ficus elastica',
+    size: 'Medium',
+    light: 'Low Light',
+    price: '35',
+    name: 'Rubber Tree'
+  },
+  {
+    id: 6, 
+    image: 'aloe-vera.png',
+    genus: 'Aloe barbadenis Miller',
+    size: 'Medium',
+    light: 'High Light',
+    price: '15',
+    name: 'Aloe Vera'
+  }
+
+];
